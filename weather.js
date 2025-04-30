65bb424f95d2f9106625f09398cf57b6640a06c9
yj901 <901duwls@gmail.com>
2025-04-30 18:37:10 +0900

    commit2

--- weather.js
+++ weather.js
@@ -1,1 +1,41 @@
 lucide.createIcons();
+
+Chart.register(ChartDataLabels);
+let myCt = document.getElementById("myChart");
+let myChart = new Chart(myCt, {
+  type: "line",
+  data: {
+    labels: ["12시", "15시", "18시", "21시", "24시", "3시"],
+    datasets: [
+      {
+        label: "temp",
+        data: [10, 20, 30, 40, 20, 30],
+        fill: true,
+        backgroundColor: "transparent",
+        borderColor: "rgba(255,255,255,0.7)",
+        pointBackgroundColor: " white",
+        tension: 0.2,
+        borderWidth: 1,
+        maxBarThickness: 1,
+        cubicInterpolationMode: "default",
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+    plugins: {
+      legend: { display: false },
+      tooltip: { enabled: false },
+      datalabels: {
+        color: "#fff",
+        align: "end",
+        anchor: "end",
+        font: { size: 12 },
+        formatter: (v) => v + "°",
+      },
+    },
+    scales: {
+      x: {},
+    },
+  },
+});
